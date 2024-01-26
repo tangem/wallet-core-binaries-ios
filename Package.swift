7c4de6d3c54e132db9cee35574f6f44ef925f342
Andrey Fedorov <m3g0byt3@gmail.com>
2024-01-27 02:18:58 +0300

    Add `SwiftProtobuf` as a dependency

--- Package.swift
+++ Package.swift
@@ -5,12 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "TangemWalletCore",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TangemWalletCore",
             targets: ["TangemWalletCore"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
