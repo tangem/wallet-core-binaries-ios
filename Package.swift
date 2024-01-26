13c38acc843b2ec3d97dca415a1df1140fc3a86f
Andrey Fedorov <m3g0byt3@gmail.com>
2024-01-27 02:03:38 +0300

    Add preliminary SPM support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TangemWalletCore",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TangemWalletCore",
+            targets: ["TangemWalletCore"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "TangemWalletCore",
+            path: "WalletCore.xcframework"
+        ),
+    ]
+)
