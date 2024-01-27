a3210091acbc9fa6023af47743475d7452b9dc81
Andrey Fedorov <m3g0byt3@gmail.com>
2024-01-27 03:02:52 +0300

    Add third party dependency

--- Package.swift
+++ Package.swift
@@ -21,8 +21,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(
+        .target(
             name: "TangemWalletCore",
+            dependencies: [
+                .target(name: "TangemWalletCoreXCFramework"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+            ]
+        ),
+        .binaryTarget(
+            name: "TangemWalletCoreXCFramework",
             path: "WalletCore.xcframework"
         ),
     ]
