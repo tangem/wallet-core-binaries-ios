816707d02dc44fbc6d6b403c2eff80c00151044d
Andrey Fedorov <m3g0byt3@gmail.com>
2024-01-29 04:36:00 +0300

    Use protobuf from enable-library-evolution-mode branch

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/m3g0byt3/swift-protobuf.git", branch: "enable-library-evolution-mode"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
