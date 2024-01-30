5206c32e191a7b94a99b790f884b13026d3aa765
Andrey Fedorov <m3g0byt3@gmail.com>
2024-01-30 21:53:39 +0300

    Add dummy target and use external protobuf

--- Package.swift
+++ Package.swift
@@ -10,24 +10,26 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "TangemWalletCoreBinaries",
-            targets: ["TangemWalletCoreBinaries"]
-        ),
-        .library(
-            name: "SwiftProtobuf",
-            targets: ["SwiftProtobufBinaries"]
+            name: "_TangemWalletCoreWrapperDoNotImport",
+            targets: ["_TangemWalletCoreWrapperDoNotImport"]
         ),
     ],
+    dependencies: [
+        .package(path: "../swift-protobuf-binaries"),
+    ],
     targets: [
-        .binaryTarget(
-            name: "TangemWalletCoreBinaries",
-            url: "https://github.com/tangem/wallet-core-binaries-ios/releases/download/3.2.4-tangem2/WalletCore.xcframework.zip",
-            checksum: "193e7146258da7d6f26fd5156dfd9ffaca5ec947ebb31f0cbb412f2322fab8b7"
+        .target(
+            name: "_TangemWalletCoreWrapperDoNotImport",
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf-binaries"),
+                .byName(name: "WalletCoreBinaries")
+            ],
+            path: "WalletCore"
         ),
         .binaryTarget(
-            name: "SwiftProtobufBinaries",
-            url: "https://github.com/tangem/wallet-core-binaries-ios/releases/download/3.2.4-tangem2/SwiftProtobuf.xcframework.zip",
-            checksum: "ab9641ea312dd8f9a07e3ac9cacc169b15c7d17bf2a01131ac1c28df0faa08d9"
+            name: "WalletCoreBinaries",
+            url: "https://github.com/tangem/wallet-core-binaries-ios/releases/download/3.2.4-tangem2/WalletCore.xcframework.zip",
+            checksum: "193e7146258da7d6f26fd5156dfd9ffaca5ec947ebb31f0cbb412f2322fab8b7"
         ),
     ]
 )
