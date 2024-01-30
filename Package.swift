7447999aaacc9f936611fddce26a2ea8d39169d5
Andrey Fedorov <m3g0byt3@gmail.com>
2024-01-30 17:07:22 +0300

    IOS-5792: Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,11 +21,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TangemWalletCoreBinaries",
-            path: "WalletCore.xcframework"
+            url: "https://github.com/tangem/wallet-core-binaries-ios/releases/download/3.2.4-tangem2/WalletCore.xcframework.zip",
+            checksum: "193e7146258da7d6f26fd5156dfd9ffaca5ec947ebb31f0cbb412f2322fab8b7"
         ),
         .binaryTarget(
             name: "SwiftProtobufBinaries",
-            path: "SwiftProtobuf.xcframework"
+            url: "https://github.com/tangem/wallet-core-binaries-ios/releases/download/3.2.4-tangem2/SwiftProtobuf.xcframework.zip",
+            checksum: "ab9641ea312dd8f9a07e3ac9cacc169b15c7d17bf2a01131ac1c28df0faa08d9"
         ),
     ]
 )
